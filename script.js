02d763e52a81949a251adb811fc878f73a5a3f83
Naftal Rainer <nrainer001@gmail.com>
2022-12-21 23:31:46 +0300

    feat: Add buttons style: Style bio

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-para = document.getElementById('descr')
-console.log(para)
+//  = document.getElementsByClassName('para')
+// console.log(para)
+
+
+
+setInterval(mySliderDisplay, 7000);
+
+let i = 0
+function mySliderDisplay() {
+    var para = document.getElementById('bio').getElementsByTagName('p')
+    console.log(para[0].innerHTML);
+    
+    document.getElementById('displ').textContent = para[i].innerHTML;
+    
+    i += 1;
+    if(i == para.length){
+        i = 0;
+    }
+}
