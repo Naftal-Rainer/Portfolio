31e5ae9d8e2da8cf0da47cce5ca3bb3b274d4b6b
Naftal Rainer <nrainer001@gmail.com>
2022-12-26 13:31:43 +0300

    feat: Add event listener to Experience button

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ setInterval(mySliderDisplay, 7000);
 let i = 0
 function mySliderDisplay() {
     var para = document.getElementById('bio').getElementsByTagName('p')
-    console.log(para[0].innerHTML);
+    // console.log(para[0].innerHTML);
     
     document.getElementById('displ').textContent = para[i].innerHTML;
     
@@ -20,3 +20,31 @@ function mySliderDisplay() {
         i = 0;
     }
 }
+
+
+let butt = document.getElementById('additionalButtons').getElementsByTagName('a');
+
+var exp = butt[1]
+
+exp.addEventListener('click', () => {
+    var descr = document.getElementById('descr');
+    descr.style.display = 'none';
+    var addButt = document.getElementById('additionalButtons');
+    addButt.style.position = 'absolute';
+    addButt.style.flexDirection = 'column';
+    addButt.style.width = '8%';
+    // addButt.style.float = 'right';
+    // addButt.style.justifyItems = 'flex-end';
+    // addButt.style.margin = 'auto';
+    addButt.style.border = '1px solid white';
+
+
+    var butElements = addButt.getElementsByTagName('a')
+    // butElements.style.width = '30%';
+    // butElements.position = 'absolute';
+});
+
+
+// for (i = 0; i < exp.length; i++) {
+//     console.log(exp[i].textContent);`
+// }
